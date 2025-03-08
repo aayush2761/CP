500878e6d5f25b65e59df15241bbd523715bbd32
Aayush Gupta <aayush2705gupta@gmail.com>
2025-03-08 11:17:46 +0530

    CP31

---
+++ bitMasking/jobAssignment.cpp
@@ -0,0 +1,7 @@
+// Let there be N workers and N jobs. Any worker can be assigned to perform any job,
+//  incurring some cost that may vary depending on the work-job assignment. 
+//  It is required to perform all jobs by assigning exactly one worker to each job 
+//  and exactly one job to each agent in such a way that the total cost of the assignment is minimized.
+
+
+
